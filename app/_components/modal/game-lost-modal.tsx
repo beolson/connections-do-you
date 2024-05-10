9148b837deed8f388d059d0db575d5d92a8cafa3
Brian Olson <beolson@hero4hire.com>
2024-05-10 08:37:43 -0500

    update

--- app/_components/modal/game-lost-modal.tsx
+++ app/_components/modal/game-lost-modal.tsx
@@ -12,13 +12,29 @@ type GameLostModalProps = {
 export default function GameLostModal(props: GameLostModalProps) {
   return (
     <GameModal isOpen={props.isOpen} onClose={props.onClose}>
-      <div className="flex flex-col items-center justify-center px-12">
-        <h1 className="text-black text-3xl font-black my-4 ml-4">
-          {"Next time!"}
-        </h1>
+      <div className="items-center justify-center px-2">
+
         <hr className="mb-2 md:mb-4 w-full"></hr>
-        <GuessHistory guessHistory={props.guessHistory} />
-        <ControlButton text="Exit" onClick={props.onClose} />
+        <div className="float-right"><GuessHistory guessHistory={props.guessHistory} /></div>
+
+        <p className="text-black mb-4">
+          Dana</p>
+        <p className="text-black mb-4">
+          Throughout our relationship, I have been constantly amazed at your strength and your 
+          willingness to sacrifice for the good of others. Your empathy shines through.
+        </p>
+        <p className="text-black mb-4">
+          Our weekend morning coffee is the highlight of my week.  Being able to talk with 
+          someone like I can with you is a blessing and one I am grateful for.
+        </p>
+        <p className="text-black mb-4">
+          All of this, and so much more is why I love you - and that love grows stronger.
+        </p>
+        <p className="text-black mb-4">
+          Getting to know you and the girls has been one of the best experiences of my life.  
+          I know my place is here with you.  Let’s make this official.  <b>Will you Marry Me?</b>
+
+        </p>
       </div>
     </GameModal>
   );
--- app/_components/modal/game-won-modal.tsx
+++ app/_components/modal/game-won-modal.tsx
@@ -14,23 +14,31 @@ export default function GameWonModal(props: GameWonModalProps) {
   return (
     <GameModal isOpen={props.isOpen} onClose={props.onClose}>
       <div className="items-center justify-center px-2">
-        <h1 className="text-black text-4xl font-black my-4 ml-4">
-          {props.perfection}
-        </h1>
+
         <hr className="mb-2 md:mb-4 w-full"></hr>
-        <h2 className="text-black mb-8">{"You've won the game!"}</h2>
+
 
         <div className="float-right"><GuessHistory guessHistory={props.guessHistory} /></div>
-        
-        <p>
-        Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-        Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-        Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-        Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-        Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test 
-        Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test s
+
+        <p className="text-black mb-4">
+          Dana</p>
+        <p className="text-black mb-4">
+          Throughout our relationship, I have been constantly amazed at your strength and your 
+          willingness to sacrifice for the good of others. Your empathy shines through.
         </p>
-        <ControlButton text="Exit" onClick={props.onClose} />
+        <p className="text-black mb-4">
+          Our weekend morning coffee is the highlight of my week.  Being able to talk with 
+          someone like I can with you is a blessing and one I am grateful for.
+        </p>
+        <p className="text-black mb-4">
+          All of this, and so much more is why I love you - and that love grows stronger.
+        </p>
+        <p className="text-black mb-4">
+          Getting to know you and the girls has been one of the best experiences of my life.  
+          I know my place is here with you.  Let’s make this official.  <b>Will you Marry Me?</b>
+
+        </p>
+
       </div>
     </GameModal>
   );
